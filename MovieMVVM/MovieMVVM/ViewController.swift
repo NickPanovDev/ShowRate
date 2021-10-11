0f520724286b7f3a28d7d95a8125d6af5729dd8f
NickDev <tjvodolaz@mail.ru>
2021-10-11 12:32:36 +0300

    init

---
+++ MovieMVVM/MovieMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMVVM
+//
+//  Created by Николай Панов on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
